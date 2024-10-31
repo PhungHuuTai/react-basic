4f0e43760922d064925b8af7bd84645ff58b33e6
PhungHuuTai <phunghuutai7@gmail.com>
2024-10-31 22:10:55 +0700

    Redux basic

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { withRouter} from 'react-router';
+import { withRouter } from 'react-router';
 import Color from "../HOC/Color";
+import image from "../../assets/images/cat.jpg";
+import { connect } from "react-redux";
 
 class Home extends React.Component {
     componentDidMount() {
@@ -8,14 +10,57 @@ class Home extends React.Component {
         //     this.props.history.push('/todos')
         // }, 3000)
     }
+
+    handleDeleteUser = (user) => {
+        this.props.deleteUserRedux(user);   
+    }
+
+    handleCreateUser = () => {
+        this.props.addUserRedux();
+    }
+
     render() {
-        return( 
-            <div>
-                Hello world from Homepage
-            </div>
+        console.log('>>> check props redux: ', this.props.dataRedux);
+        let listUsers = this.props.dataRedux;
+        return (
+            <>
+                <div>
+                    Hello world from Homepage
+                </div>
+                <div>
+                    <img src={image} style={{ width: '200px', height: '200px', marginTop: '20px' }} />
+                </div>
+                <div>
+                    {listUsers && listUsers.length > 0 &&
+                        listUsers.map((item, index) => {
+                            return (
+                                <div key={item.id}>
+                                    {index + 1} - {item.name} 
+                                    &nbsp; <span onClick={() => this.handleDeleteUser(item)}>x</span>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <button onClick={()=>this.handleCreateUser()}>Add new</button>
+            </>
+
         )
     }
 }
 
+const mapStateToProps = (state) => {
+    return {
+        dataRedux: state.users
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deleteUserRedux: (userDelete) => dispatch({ type: 'DELETE_USER', payload: userDelete }),
+        addUserRedux: () => dispatch({ type: 'CREATE_USER'}),
+    }
+}
+
 // export default withRouter(Home);
-export default Color(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Color(Home));
